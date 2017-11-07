02890c274053e9e2348dfb66f55d430eb62ea17f
JakeGreer <Jakegreer11@gmail.com>
2017-11-07 13:49:05 -0800

    added file structure

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Jumbotron.css';
+
+const Header = () => (
+
+    <header>
+        <div className="jumbotron text-center">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1><i className="fa fa-newspaper-o"></i> New York Times Search</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+)
+
+
+export default Header;
+

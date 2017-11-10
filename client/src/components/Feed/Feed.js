2a131c9d6b6aead3a568d9fb8e5889739321b8a3
theRealScoobaSteve <stephensimone@live.com>
2017-11-09 20:55:46 -0800

    added more styling to Related

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -3,34 +3,41 @@ import {Row, Col} from "../Grid"
 import "./Feed.css"
 import Card from "../Card"
 
-// class Feed extends Component {
-//     constructor()
-//     {
-//         super()
-//         this.state = 
-//         {
-//             feedData: []
-//         }
-//     }
+class Feed extends Component {
+    constructor()
+    {
+        super()
+        this.state = 
+        {
+            feedData: []
+        }
+    }
 
-//     render() {
-//         return (
+    componentDidMount() {
 
-//                     /*The below code will be implemented later*/
-//                     /* <div id="main-feed">
-//                         {this.state.feedData ? this.state.feedData.map(e =>
-//                         {
-//                             <Card title={e.title} subtitle={e.subtitle}
-//                              text={e.text} firstText="View Project" secondText="View Profile"
-//                              />
-//                         } :
+    }
 
-//                         <h1>No Projects Available</h1>
-                             
-//                         }
-//                     </div> */
-//         )
-//     }
-// }
+    projectLink = e =>
+    {
+        e.preventDefault();
+
+    }
+
+    render() {
+        return (
+
+            <div id="main-feed">
+                {this.state.feedData ? this.state.feedData.map(e =>
+                (
+                    <Card title={e.title} subtitle={e.subtitle}
+                        text={e.text} firstText="View Project" secondText="View Profile"/>
+                ))
+                :
+                (<h1>No Projects Available</h1>)
+                }
+            </div> 
+        )
+    }
+}
     
-// export default Feed
+export default Feed

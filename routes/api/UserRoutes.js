bb47e3d972a4ce3d45127dab38822c1618383702
Corey Rodems <coreyrodems@gmail.com>
2017-11-21 20:15:32 -0800

    added passport

--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -2,32 +2,38 @@ const router = require("express").Router();
 var auth = require('../../controllers/authController.js');
 var posts = require("../../controllers/postController.js");
 var db = require("../../models");
-
+const isLoggedIn =(req,res,next) => {
+  return req.isAuthenticated() ? next() : res.send(403, "not authenticated")
+}
 
 // Matches with "/api/user"
+
   router
     .route("/sign-up")//path /api/user/sign-up
     .get(auth.register)
     .post(auth.doRegister)
 
+  router
+    .route("/sign-in")
+    .post(auth.doLogin);
+
+    router.use(isLoggedIn)
+
     router
-    .route("/profile/:id")//path /api/user/sign-up
+    .route("/profile/")//path /api/user/sign-up
     .get(auth.findOne)
     .post(posts.updateUser)
 
 
-  router
-  .route("/browse")
-  .get(posts.findAll)
-  .post(posts.create)
+    router
+      .route("/browse")
+      .get(posts.findAll)
+      .post(posts.create)
 
-  router 
-  .route("/profile")
-  .get(auth.findOne);
 
-  router
-  .route("/sign-in")
-  .post(auth.doLogin);
+
 
 module.exports = router;
+
+
 

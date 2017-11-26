c79d5485ba4822877fdfc420abec71c7da545130
theRealScoobaSteve <stephensimone@live.com>
2017-11-25 19:05:30 -0800

    idk

--- controllers/postController.js
+++ controllers/postController.js
@@ -56,5 +56,5 @@ module.exports = {
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  },
+  }
 };

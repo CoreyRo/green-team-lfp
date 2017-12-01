137b29214c36848f236efb90db85c69278135567
JakeGreer <Jakegreer11@gmail.com>
2017-11-30 16:47:08 -0800

    added the project view page

--- client/src/components/MyInfo/MyInfo.js
+++ client/src/components/MyInfo/MyInfo.js
@@ -7,7 +7,7 @@ const MyInfo = props => {
   console.log(props)
   return (<Row>
     <Row>
-    <h3>About me</h3>
+    <h3 className="info-headers">About me</h3>
       <Col size="sm-12">
       <div className="aboutMe profileAboutDiv">
         <p>{props.props.about || "About me...."}</p>
@@ -15,7 +15,7 @@ const MyInfo = props => {
       </Col>
     </Row>
     <Row>
-      <h3>My Projects</h3>
+      <h3 className="info-headers">My Projects</h3>
       <Col size="sm-12">
       <div className="myProjects profileAboutDiv">
           {props.props.projects.length >= 1 ? props.props.projects.map((project, i) => <span key={i}>{i=== props.props.projects.length - 1 ? project : project + ", "} </span>  
@@ -25,7 +25,7 @@ const MyInfo = props => {
       </Col>
     </Row>
     <Row>
-    <h3>Joined Projects</h3>
+    <h3 className="info-headers">Joined Projects</h3>
       <Col size="sm-12">
       <div className="myJoined profileAboutDiv">
         {props.props.joined.length >= 1 ? props.props.joined.map((join,i) => <span key={i}>{i=== props.props.joined.length - 1 ? join : join + ", "}</span>  
--- client/src/pages/Project-View/Project-View.js
+++ client/src/pages/Project-View/Project-View.js
@@ -12,24 +12,61 @@ class Project extends Component {
         title: "",
         author: "",
         description: "",
-        skills: []
+        user: []
+
     }
 
     componentDidMount() {
         axios.get("/api/user/project")
         .then((res) => {
             console.log(res);
+            this.state.user = res;
+            console.log("This.state.user ", this.state.user);
+
         })
     }
 
     render() {
         return (
+            <div>
+            <Navbar />
             <Container>
                 <Row>
-                <Col size="md-4" className="left-col">Left</Col>
-                <Col size="md-8" className="right-col">Right</Col>
+                <Col size="md-6 left-col">
+                <h3>Project Title</h3>
+                <i class="fa fa-lg fa-thumbs-o-up like-btn" aria-hidden="true">Like</i>
+                <i class="fa fa-lg fa-archive save-btn" aria-hidden="true">Save</i>
+                    <h6 className="small-headers">Details</h6>
+                    <p className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <h6 className="small-headers">Skills Desired</h6>
+                    <span className="skills">Javascript</span>
+                    <span className="skills">React</span>
+                    <span className="skills">Node</span>
+                    <span className="skills">Passport</span>
+
+                    <h6 className="small-headers">Contact</h6>
+                    <a className="icons" href="#">
+                        <i className="fa fa-2x fa-envelope-o"></i>
+                    </a>
+                    <a className="icons" href="#">
+                        <i className="fa fa-2x fa-comments"></i>
+                    </a>
+                </Col>
+
+
+                <Col size="md-5 right-col">
+                <h3>Project Owner: <a className="username" href="/profile/">Username</a></h3>
+                    <h6 className="small-headers">Bio</h6>
+                    <p className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                    <h6 className="small-headers">Skills</h6>
+                    <span className="skills">Angular</span>
+                    <span className="skills">C#</span>
+                    <span className="skills">HTML</span>
+                    <span className="skills">CSS</span>
+                </Col>
                 </Row>
             </Container>
+            </div>
         )
     }
 

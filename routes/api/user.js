22ba003992afecce6e913543d41a35fc7905ba60
theRealScoobaSteve <stephensimone@live.com>
2017-12-02 12:14:15 -0800

    Fixed routing structure and add forgot pass button

---
+++ routes/api/user.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const posts = require("../../controllers/postController.js");
+const auth = require("../../controllers/authController.js")
+
+
+// Prefix is with "/api"
+
+//Route for signing up
+router
+  .route("/sign-up")//path /api/sign-up
+  .get(auth.register)
+  .post(auth.doRegister)
+
+  
+
+//Route for signing in
+router
+  .route("/sign-in")
+  .post(auth.doLogin)
+
+module.exports = router;
+
+
+

496da134e410b487735f4e6a450c8e5920a46cfc
theRealScoobaSteve <stephensimone@live.com>
2017-12-02 12:42:01 -0800

    Got EveryThing Linked upped

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserHistory, BrowserRouter as Router, Route } from 'react-router-dom
 import logo from './logo.svg';
 import './App.css';
 import Profile from './pages/Profile';
-import viewProfile from "./pages/viewProfile";
+import ViewProfile from "./pages/ViewProfile";
 import Browse from './pages/Browse';
 import Navbar from './components/Navbar';
 import Wrapper from './components/Wrapper';
@@ -14,21 +14,25 @@ import SignIn from "./pages/Sign-In";
 import Splash from "./pages/Splash";
 import Logout from "./components/Logout";
 import Project from "./pages/Project-View";
+import Recovery from './pages/Recovery'
 
 class App extends Component {
   render() {
-    return <Router>
+    return (
+    <Router>
         <Wrapper>
           <Route exact path="/" component={Splash} />
           <Route exact path="/myProfile/" component={Profile} />
-          <Route path="/profile/" component={viewProfile} />
+          <Route path="/profile/" component={ViewProfile} />
           <Route exact path="/browse" component={Browse} />
           <Route exact path="/sign-up" component={Login} />
           <Route exact path="/sign-in" component={SignIn} />
           <Route exact path="/Logout" component={Logout} />
+          <Route exact path='/recovery' component={ Recovery } />
           <Route exact path="/project-view" component={Project} />
         </Wrapper>
-      </Router>;
+      </Router>
+    )
   }
 }
 

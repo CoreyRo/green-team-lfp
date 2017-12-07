dfa55c771cab2ca1095d89c33ec6be540a10ef98
Corey Rodems <coreyrodems@gmail.com>
2017-12-06 17:20:20 -0800

    readded models

---
+++ models/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    Post: require("./post"),
+    User: require("./User")
+  };
+  
+
+
+  const mongoose = require("mongoose");
+  var passportLocalMongoose = require("passport-local-mongoose");
+  const Schema = mongoose.Schema;
+
+
+

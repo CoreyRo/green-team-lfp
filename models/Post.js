3fb07f6fbcb3aaf3d1129697f5dd1e6af42a64a9
Corey Rodems <coreyrodems@gmail.com>
2017-12-06 17:14:08 -0800

    testing new commit

--- models/Post.js
+++ models/Post.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-  userId: { type: String, required: true},
+  userId: { type: String, required: true },
   title: { type: String, required: true },
   skills: { type: Array, required: true },
-  description: {type: String, required: true},
+  description: { type: String, required: true },
   dateAdded: { type: Date, default: Date.now }
 });
 
--- models/User.js
+++ models/User.js
@@ -1,27 +1,21 @@
-const mongoose = require("mongoose");
-var passportLocalMongoose = require('passport-local-mongoose');
-const Schema = mongoose.Schema;
-
 const UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
-  username: {type: String, required: true},
-  displayName: {type: String},
-  password: {type: String, required: true},
-  location: {type: String},
-  skills: {type: Array},
-  joined: {type: Array},
-  projects: {type: Array},
-  imageURL: {type: String},
-  about: {type: String}
+  username: { type: String, required: true },
+  displayName: { type: String },
+  password: { type: String, required: true },
+  location: { type: String },
+  skills: { type: Array },
+  joined: { type: Array },
+  projects: { type: Array },
+  imageURL: { type: String },
+  about: { type: String }
 });
 
 //takes care of salting and hashing the passpword for us
 UserSchema.plugin(passportLocalMongoose);
 
-
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
-

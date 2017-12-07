a8857b3403a678a23b3b98fa9af8104ba8eab5c0
Corey Rodems <coreyrodems@gmail.com>
2017-12-06 17:16:52 -0800

    readded models

---
+++ models/Post.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  skills: { type: Array, required: true },
+  description: { type: String, required: true },
+  dateAdded: { type: Date, default: Date.now }
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;

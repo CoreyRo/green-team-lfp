05d67a1405ac855fe52a4653356be18c4c6a1a69
JakeGreer <Jakegreer11@gmail.com>
2017-12-07 19:05:56 -0800

    updated

--- models/User.js
+++ models/User.js
@@ -14,6 +14,7 @@ const UserSchema = new Schema({
   joined: { type: Array },
   projects: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
   imageURL: { type: String },
+  messages: { type: Array },
   about: { type: String }
 });
 
